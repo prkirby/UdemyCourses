d97b273376a5b45e2e37b1f4317801dd7181fdff
Paul Kirby <paul@undergroundweblab.com>
2018-03-12 21:48:04 -0500

    React Basics Youtube App

--- ModernReactRedux/ReduxSimpleStarter/src/index.js
+++ ModernReactRedux/ReduxSimpleStarter/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
-
+import VideoList from './components/video-list';
 const API_KEY = 'AIzaSyDF9iVbx9TL-hUh-kIFrx6dv34AuG60RJo';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = { videos: [] };
+
+      YTSearch({key: API_KEY, term: 'arduino'}, (videos) => {
+        this.setState({ videos });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it on the page (in the dom)
 ReactDOM.render(<App />, document.querySelector('.container'));
