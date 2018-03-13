d3ae3029621cd153119630e5d4bb174212d874b6
Paul Kirby <paul@undergroundweblab.com>
2018-03-13 09:39:56 -0500

    Section 3 Modern React

--- ModernReactRedux/ReduxSimpleStarter/src/index.js
+++ ModernReactRedux/ReduxSimpleStarter/src/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 const API_KEY = 'AIzaSyDF9iVbx9TL-hUh-kIFrx6dv34AuG60RJo';
 
 
@@ -11,18 +13,34 @@ class App extends Component {
   constructor(props) {
       super(props);
 
-      this.state = { videos: [] };
+      this.state = {
+        videos: [],
+        selectedVideo: null
+      };
 
-      YTSearch({key: API_KEY, term: 'arduino'}, (videos) => {
-        this.setState({ videos });
+      this.videoSearch('arduino');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
       });
+    });
   }
 
   render() {
+
+    const videoSearch = _.debounce(term => {this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
