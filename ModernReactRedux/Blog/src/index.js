2f6a76e4c2c15b71b6234e438d2c971dac958f14
Paul Kirby <paul@undergroundweblab.com>
2018-03-15 11:07:44 -0500

    working on blog

--- ModernReactRedux/Blog/src/index.js
+++ ModernReactRedux/Blog/src/index.js
@@ -2,27 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom'
-;
-import App from './components/app';
+import { BrowserRouter, Route } from 'react-router-dom';
 import reducers from './reducers';
+import PostsIndex from './components/posts-index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() { return <div>Hello</div>; }
-}
-
-class Goodbye extends React.Component {
-  render() { return <div>GoodBye!!</div>; }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello}/>
-        <Route path="/goodbye" component={Goodbye}/>
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
