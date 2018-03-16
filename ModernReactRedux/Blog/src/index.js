fc4e209eda372638fdd6be4aa4ddf6425a70462f
Paul Kirby <paul@undergroundweblab.com>
2018-03-15 19:19:13 -0500

    Working on blog app

--- ModernReactRedux/Blog/src/index.js
+++ ModernReactRedux/Blog/src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ReduxPromise from 'redux-promise';
 import reducers from './reducers';
-import PostsIndex from './components/posts-index';
+import PostsIndex from './containers/posts-index';
+import PostsNew from './containers/posts-new';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
